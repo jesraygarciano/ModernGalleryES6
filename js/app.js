76fe5847904add2ae70224551856d40b5245dd8a
Jesray Garciano <jesray.garciano@gmail.com>
2018-02-26 22:18:45 +0800

    Finishing up

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// const current = document.querySelector('#current');
+// const imgs = document.querySelectorAll('.imgs img');
+
+const [current, imgs] = [document.querySelector('#current'), document.querySelectorAll('.imgs img')];
+const opacity = 0.4;
+// imgs.forEach(img =>
+//  img.addEventListener('click', e => (current.src =
+//   e.target.src))
+//  );
+
+// Set first img opacity
+imgs[0].style.opacity = opacity;
+
+imgs.forEach(img => img.addEventListener('click',
+ imgClick));
+
+function imgClick(e) {
+
+	// Reset the opacity
+	imgs.forEach(img => (img.style.opacity = 1));
+
+	// Change current image to src of clicked image
+	current.src = e.target.src;
+
+	// Add fade in class
+	current.classList.add('fade-in');
+
+	//Remove fade-in class after .5 seconds
+	setTimeout(() => current.classList.remove('fade-in'),
+		500);
+
+	// Change the opacity to opacity var
+	e.target.style.opacity = opacity;
+}
