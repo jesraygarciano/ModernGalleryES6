0612846e35711f46bfe0918bf6badd66a174002a
Jesray Garciano <jesray.garciano@gmail.com>
2018-03-03 20:47:36 +0800

    minor fixes

--- js/app.js
+++ js/app.js
@@ -12,23 +12,23 @@ const opacity = 0.4;
 imgs[0].style.opacity = opacity;
 
 imgs.forEach(img => img.addEventListener('click',
- imgClick));
+imgClick));
 
 function imgClick(e) {
 
-	// Reset the opacity
-	imgs.forEach(img => (img.style.opacity = 1));
+// Reset the opacity
+imgs.forEach(img => (img.style.opacity = 1));
 
-	// Change current image to src of clicked image
-	current.src = e.target.src;
+// Change current image to src of clicked image
+current.src = e.target.src;
 
-	// Add fade in class
-	current.classList.add('fade-in');
+// Add fade in class
+current.classList.add('fade-in');
 
-	//Remove fade-in class after .5 seconds
-	setTimeout(() => current.classList.remove('fade-in'),
-		500);
+//Remove fade-in class after .5 seconds
+setTimeout(() => current.classList.remove('fade-in'),
+	500);
 
-	// Change the opacity to opacity var
-	e.target.style.opacity = opacity;
+// Change the opacity to opacity var
+e.target.style.opacity = opacity;
 }
